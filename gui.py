6fb004f6eeab5812abb7ed91691787d8e6797d54
Ali <38077025+ATahma@users.noreply.github.com>
2019-03-03 06:26:42 -0500

    Graphical I/O - Input Menu

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit,  QFileDialog
 from PyQt5.QtGui import QIcon
 from summer import summer
 
@@ -20,21 +20,48 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.getName()
-        self.getRatio()
+        self.getMethod()
+        if self.method == 'Filename':
+            self.openFileNameDialog()
+            #self.getName()
+            self.getRatio()
+            self.saveFileDialog()
+        else:
+            print('TODO')
+
         self.show()
 
-        summer(self.name,self.ratio)
+        summer(self.name,self.ratio,self.saveName)
 
-    def getName(self):
-        name, okPressed = QInputDialog.getText(self, "File Name","FileName + extension: ", QLineEdit.Normal, "")
-        if okPressed and name != '':
-            self.name = name
+#    def getName(self):
+#        name, okPressed = QInputDialog.getText(self, "File Name","FileName + extension: ", QLineEdit.Normal, "")
+#        if okPressed and name != '':
+#            self.name = name
 
     def getRatio(self):
         ratio, okPressed = QInputDialog.getItem(self, "Get Ratio","Ratio :", self.ratioItems, 0, False)
         if ratio:
             self.ratio = float(ratio)
+
+    def getMethod(self):
+        methods = ("Filename","TextInput","Audio Input")
+        method, okPressed = QInputDialog.getItem(self, "Get input method","Input Method: ", methods, 0, False)
+        if okPressed:
+            self.method = method
+
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Text Files (*.txt)", options=options)
+        if fileName:
+            self.name = fileName
+
+    def saveFileDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        saveName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)", options=options)
+        if saveName:
+            self.saveName = saveName
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- summer.py
+++ summer.py
@@ -1,4 +1,4 @@
-def summer(filename,ratio):
+def summer(filename,ratio,savedir):
     """
     Summerizes and provides keywords and fucking heck I dont know ffs
     """
@@ -8,13 +8,13 @@ def summer(filename,ratio):
     print('____Opening File_____')
     text = open(filename).read()
     print('____File Succesfully Opened____')
-    
+
     if __name__ == "__main__":
         print(summarize(text,ratio = ratio))
         print(keywords(text, ratio= ratio))
 
     # output
-    output = open('output.txt','w+')
+    output = open(savedir,'w+')
     output.write(30 * '--' + '\n' + 'FILENAME:' + filename + '\n' + 30 * '--'  + '\n')
     output.write(sumSign + '\n')
     output.write(summarize(text,ratio = ratio) + '\n')
