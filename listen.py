28ca3e265a32f2ef9874a9583b499be72eb6c3d5
Matteo Gisondi <matteo.gisondi@gmail.com>
2019-03-03 10:24:15 -0500

    Add files via upload

--- listen.py
+++ listen.py
@@ -1,29 +1,30 @@
 import speech_recognition as sr
 from recognize_speech import recognize_speech
 from write import write
+def file(fname):
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+    try:
+        speech = recognize_speech(sr, recognizer, microphone, fname)
+        return write(speech)
+    except FileNotFoundError:
+        return 'file {} does not exist'.format(fname)
 
-recognizer = sr.Recognizer()
-microphone = sr.Microphone()
-
-while True:
-    usr = input('file or microphone (x to exit): ').lower()
-    file, mic = ['f', 'file'], ['m', 'mic', 'micro', 'microphone']
-    if usr in file:
-        fname = input('file name: ')
+def mic():
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+    speech = ''
+    while True:
         try:
-            speech = recognize_speech(sr, recognizer, microphone, fname)
-            write(speech)
-        except FileNotFoundError:
-            print('file {} does not exist'.format(fname))
-    elif usr in mic:
-        while True:
-            try:
-                phrase = recognize_speech(sr, recognizer, microphone)
-                if phrase:
-                    write(phrase)
-            except KeyboardInterrupt:
-                break
-    elif usr == 'x':
-        exit()
-    else:
-        print('invalid input')
+            phrase = recognize_speech(sr, recognizer, microphone)
+            if phrase:
+                speech += phrase + ' '
+                print(phrase)
+        except KeyboardInterrupt:
+            if speech:
+                write(speech)
+            break
+
+if __name__ == '__main__':
+    # file('harvard.wav')
+    mic()
